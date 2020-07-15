0e9d792bdbc7252501d49e66d1931a59b39af43b
landry333 <57639988+Landry333@users.noreply.github.com>
2020-07-15 02:31:33 -0400

    display real number of infected cases
    having the real number of infected cases fetched from api and displayed on card of infected cases

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,8 +3,10 @@ import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
 import styles from './Cards.module.css';
 
-const Cards =(props) => {
-    console.log(props);
+const Cards =({data:{confirmed,recovered,deaths,lastUpdate}}) => {
+    if(!confirmed){
+        return 'Loading...'
+    }
 
     return(
         <div className={styles.container}>
@@ -12,7 +14,7 @@ const Cards =(props) => {
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography variant="h5">{confirmed.value}</Typography>
                         <Typography color="textSecondary">REAL DATE</Typography>
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
