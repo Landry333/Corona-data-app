64b0669860e4251541122c37cc60e1e9c221d34f
landry333 <57639988+Landry333@users.noreply.github.com>
2020-07-16 22:52:36 -0400

    Actual date added on infected card
    The current date corresponding to number of infected cases displayed was fetched from api and displayed on card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -16,7 +16,7 @@ const Cards =({data:{confirmed,recovered,deaths,lastUpdate}}) => {
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">{confirmed.value}</Typography>
                         <CountUp start={0} end={confirmed.value} duration={2.75} separator="," />
-                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
