0fe504ef12212dbccf5257f360395d660fa19ed1
landry333 <57639988+Landry333@users.noreply.github.com>
2020-07-17 23:59:00 -0400

    Display of line chart added
    Adding a line chart on the app with daily data

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -18,9 +18,34 @@ const Chart = () => {
     fetchMyAPI();
   }, []);
 
+  const lineChart = (
+    dailyData[0] ? (
+      <Line
+        data={{
+          labels: dailyData.map(({ date }) => date),
+          datasets: [{
+            data: dailyData.map((data) => data.confirmed),
+            label: 'Infected',
+            borderColor: '#3333ff',
+            fill: true,
+          }, {
+            data: dailyData.map((data) => data.deaths),
+            label: 'Deaths',
+            borderColor: 'red',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            fill: true,
+          },
+          ],
+        }}
+      />
+    ) : null
+  );
+
   return (
-        <h1>Chart</h1>
-    )
+    <div className={styles.container}>
+      {lineChart}
+    </div>
+  );
 };
 
 export default Chart;
