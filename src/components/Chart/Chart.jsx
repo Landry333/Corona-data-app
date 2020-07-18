e02c41e79f325ea1b6ee112aa2695189c2ec772f
landry333 <57639988+Landry333@users.noreply.github.com>
2020-07-17 23:56:41 -0400

    Use of daily data to display chart added
    Using daily data fetched from API to display line chart and bar chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Line } from 'react-chartjs-2';
+
+import { fetchDailyData } from '../../api';
+
+import styles from './Chart.module.css';
 
 const Chart = () => {
-    return (
+  const [dailyData, setDailyData] = useState({});
+
+  useEffect(() => {
+    const fetchMyAPI = async () => {
+      const initialDailyData = await fetchDailyData();
+
+      setDailyData(initialDailyData);
+    };
+
+    fetchMyAPI();
+  }, []);
+
+  return (
         <h1>Chart</h1>
     )
-}
+};
 
 export default Chart;
